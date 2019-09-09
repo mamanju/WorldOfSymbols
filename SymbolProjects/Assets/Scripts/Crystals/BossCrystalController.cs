39102cf928c591de333b48818cf27ff110d37c48
mamanju <himazin19970630@gmail.com>
2019-09-10 00:06:39 +0900

    ゲームクリア追加

---
+++ SymbolProjects/Assets/Scripts/Crystals/BossCrystalController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class BossCrystalController : MonoBehaviour
+{
+    private GameObject clearPanel;
+    // Start is called before the first frame update
+    void Awake()
+    {
+        clearPanel = GameObject.FindObjectOfType<ClearPanelController>().gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void OnDestroy() {
+
+    }
+}
---
+++ SymbolProjects/Assets/Scripts/Pause/ClearPanelController.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ClearPanelController : MonoBehaviour
+{
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButtonDown("Circle")) {
+            SceneController.Instance.ChangeScene(SceneController.SceneName.Title);
+        }
+    }
+}
